f87eece36b61711cdb7bfbda1eac419ede0ecc76
s800952 <59623738+s800952@users.noreply.github.com>
2020-08-19 12:51:52 -0500

    Add files via upload

---
+++ GAME 3PLAYER.py
@@ -0,0 +1,158 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+#Score
+score_a = 5
+score_b = 5
+score_c = 5
+
+#Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a. shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+#Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b. shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+#Paddle C
+paddle_c = turtle.Turtle()
+paddle_c.speed(0)
+paddle_c.shape("square")
+paddle_c.color("white")
+paddle_c. shapesize(stretch_wid=1, stretch_len=5)
+paddle_c.penup()
+paddle_c.goto(0,-250)
+
+#Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = .2
+ball.dy = .2
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 5  Player B: 5  Player C: 5", align="center", font=("Courier", 24, "normal"))
+
+#Function
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+#Keyboard binging
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+    wn.listen()
+wn.onkeypress(paddle_a_down, "s")
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+    wn.listen()
+wn.onkeypress(paddle_b_up, "Up")
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+    wn.listen()
+wn.onkeypress(paddle_b_down, "Down")
+
+def paddle_c_left():
+    x = paddle_c.xcor()
+    x -= 20
+    paddle_c.setx(x)
+ 
+    wn.listen()
+wn.onkeypress(paddle_c_left, "v")
+ 
+def paddle_c_right():
+    x = paddle_c.xcor()
+    x += 20
+    paddle_c.setx(x)
+ 
+    wn.listen()
+wn.onkeypress(paddle_c_right, "b")
+
+#Main game loop
+while True:
+    wn.update()
+
+    #Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.goto(0,0)
+        ball.dy *= -1
+        score_c -= 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}  Player C: {}".format(score_a, score_b, score_c), align="center", font=("Courier", 24, "normal"))
+
+
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_a -= 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}  Player C: {}".format(score_a, score_b, score_c), align="center", font=("Courier", 24, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+        score_b -= 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}  Player C: {}".format(score_a, score_b, score_c), align="center", font=("Courier", 24, "normal"))
+
+    
+
+    #Paddle and Ball collisions
+    if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+
+    if ball.xcor() < -340 and ball.xcor() > -350 and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+
+    if ball.ycor() < -240 and ball.ycor() > -250 and (ball.xcor() < paddle_c.xcor() + 40 and ball.xcor() > paddle_c.xcor() - 40):
+        ball.sety(-240)
+        ball.dy *= -1
